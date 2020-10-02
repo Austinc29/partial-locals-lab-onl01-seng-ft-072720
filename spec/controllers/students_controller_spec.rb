06c8a74ebb51086ddbef7c92897db82f8e30742a
Austin Cummings <aac729@live.com>
2020-10-02 01:10:03 +0000

    Automatically backed up by Learn

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,11 +1,1 @@
-require 'rails_helper'
 
-RSpec.describe StudentsController, type: :controller do
-  describe 'students#index' do
-    describe 'when passed a search query on the "query" parameter' do
-      render_views
-
-      
-  end
-
-end
